dc099c400c3958d44fbbf86d087e2a6ddf652623
FacuHere <herediacorreo@hotmail.com>
2026-06-07 23:46:28 -0300

    Closes #5

--- src/server.js
+++ src/server.js
@@ -7,22 +7,18 @@ const productos = [
   { id: 2, nombre: 'Silla', categoria: 'muebles' },
   { id: 3, nombre: 'Monitor', categoria: 'electronica' },
 ];
-
 const usuarios = [
   { id: 1, nombre: 'Juan' },
   { id: 2, nombre: 'Maria' },
   { id: 3, nombre: 'Pedro' },
 ];
-
 // TODO: Define tus rutas aquí
-
 app.get('/', (req, res) => {
   res.send('Bienvenid@s a nuestro servidor Express!');
 });
 
 app.get('/productos', (req, res) => {
   const { categoria } = req.query;
-
 
   if (categoria) {
     const productosFiltrados = productos.filter(
@@ -41,9 +37,7 @@ app.get('/usuarios/:id', (req, res) => {
   }
   return res.json(usuario);
 });
-
 app.use((req, res) => {
   res.status(404).send('No se ha encontrado la ruta ingresada.');
 });
-
 module.exports = app;
